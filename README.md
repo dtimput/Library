2dbf91cf12f07089cf96db346ca6ea3f4be25788
dtimput <122069647+dtimput@users.noreply.github.com>
2023-02-08 21:21:39 -0800

    Added Basic button function

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Library
-My 8th project from The Odin Project, meant to practice more advanced JavaScript using Objects.
+
+My 8th project from The Odin Project, meant to practice more advanced JavaScript using Objects.  
+[Link to live project](https://dtimput.github.io/Library/)
--- script.js
+++ script.js
@@ -1,5 +1,79 @@
-let myLibrary = [];
+/* eslint-disable prefer-template */
+// Variables
+const webpage = document.querySelector(".container");
+const libraryContainer = document.querySelector(".library-container");
+const addBookButton = document.querySelector(".add-book-button");
+const overlay = document.querySelector(".overlay");
+const bookForm = document.querySelector(".book-form");
+const closeButton = document.querySelector(".close-overlay");
+const myLibrary = [
+  { title: "Scythe", author: "Neal Scusterman", pages: 445, isRead: true },
+];
 
-function Book() {}
+// Object Constructor
+function Book(title, author, pages, isRead) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.isRead = isRead;
+}
+// Add book function
+function addBookToLibrary(title, author, pages, isRead) {
+  const newBook = new Book(title, author, pages, isRead);
+  myLibrary.push(newBook);
+}
 
-function addBookToLibrary() {}
+// Event Listeners
+
+// Add Book Button Listener
+addBookButton.addEventListener("click", () => {
+  overlay.style.display = "block";
+  webpage.classList.add("blurred");
+});
+
+// Submit Button Listener
+bookForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const title = document.querySelector("#title").value;
+  const author = document.querySelector("#author").value;
+  const pages = document.querySelector("#pages").value;
+  const hasRead = document.querySelector(
+    'input[name="have_read"]:checked'
+  ).value;
+
+  // Adding new Book to DOM
+  const bookContainer = document.createElement("div");
+  bookContainer.classList.add("book-container");
+  libraryContainer.appendChild(bookContainer);
+
+  const bookTitle = document.createElement("div");
+  bookTitle.classList.add("book-title");
+  bookTitle.textContent = "Title: " + title;
+  const bookAuthor = document.createElement("div");
+  bookAuthor.classList.add("book-author");
+  bookAuthor.textContent = "By: " + author;
+  const bookPages = document.createElement("div");
+  bookPages.classList.add("book-pages");
+  bookPages.textContent = "Number of Pages: " + parseFloat(pages);
+  const bookHasRead = document.createElement("div");
+  bookHasRead.classList.add("book-read");
+  bookHasRead.textContent = hasRead === "true";
+
+  bookContainer.appendChild(bookTitle);
+  bookContainer.appendChild(bookAuthor);
+  bookContainer.appendChild(bookPages);
+  bookContainer.appendChild(bookHasRead);
+
+  // Clear the form after adding
+  webpage.classList.remove("blurred");
+  overlay.style.display = "none";
+  addBookToLibrary(title, author, parseFloat(pages), hasRead === "true");
+  console.table(myLibrary);
+  bookForm.reset();
+});
+
+// Close Button Listener
+closeButton.addEventListener("click", () => {
+  webpage.classList.remove("blurred");
+  overlay.style.display = "none";
+});
